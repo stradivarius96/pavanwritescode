09dca159479ea4ab64da1f73c4e3764a67d8979e
Matt Pavan <matthew.pavan@gmail.com>
2015-04-23 23:56:23 -0400

    more smtp

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var fs      = require('fs');
 var nodemailer = require("nodemailer");
-
+var app = express();
 
 var smtpTransport = nodemailer.createTransport("SMTP",{
 service: "Gmail",
@@ -14,6 +14,26 @@ pass: "Letmein1234%"
 });
 
 
+app.get('/',function(req,res){
+res.sendfile('index.html');
+});
+app.get('/send',function(req,res){
+var mailOptions={
+to : req.query.to,
+subject : req.query.subject,
+text : req.query.text
+}
+console.log(mailOptions);
+smtpTransport.sendMail(mailOptions, function(error, response){
+if(error){
+console.log(error);
+res.end("error");
+}else{
+console.log("Message sent: " + response.message);
+res.end("sent");
+}
+});
+});
 
 /**
  *  Define the sample application.
