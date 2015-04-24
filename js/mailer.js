724ea9491f89553f22fb43c2c7036a916d4f07b4
Matt Pavan <matthew.pavan@gmail.com>
2015-04-23 23:28:47 -0400

    mailer changes

---
+++ js/mailer.js
@@ -0,0 +1,16 @@
+$(function(){
+    var from,to,subject,text;
+    $("#send-email").click(function(){      
+        to= "fredk1018@gmail.com";
+        subject="NO-REPLY From FredKhoury.me, ("+$("#phone").val()+")"+$("#name").val()+" <"+$("#email").val()+"> has sent you a message."
+        text=$("#message").val();
+        $("#message").text("Sending E-mail...Please wait");
+        $.get("/send",{to:to,subject:subject,text:text},function(data){
+        if(data=="sent")
+        {
+            console.log('success');
+        }
+
+});
+    });
+});
