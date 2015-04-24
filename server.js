7ffda4c932f92d15b7e51d03a674df7e70a1f1e5
Matt Pavan <matthew.pavan@gmail.com>
2015-04-24 19:05:09 -0400

    email attempt 134123

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
-var nodemailer = require('nodemailer');
+//var nodemailer = require('nodemailer');
 
 // create reusable transporter object using SMTP transport
 
