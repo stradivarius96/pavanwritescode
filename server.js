d5bf60fbaf66d0ef21293f6235d1e12c11d3b7d5
Matt Pavan <matthew.pavan@gmail.com>
2015-04-25 18:57:58 -0400

    Mail attempt number billion

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ sendgrid.send(email);*/
         email.addTo("matthew.pavan@gmail.com");
         email.setFrom("noreply@pavanwritescode.com");
         email.setSubject("Form Submission from PavanWritesCode.com");
-        email.setHtml("and easy to do anywhere, even with Node.js" + params);
+        email.setHtml("and easy to do anywhere, even with Node.js" + params.toString());
 
         sendgrid.send(email);
     }
